75ee762880781f3b7920c956f58307b3f9602cc1
Riya Javheri <riyajavheri0227@gmail.com>
2026-08-06 19:33:47 +0530

    Configure static files

--- website/settings.py
+++ website/settings.py
@@ -126,6 +126,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
